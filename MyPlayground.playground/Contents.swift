fe260323fed0bb713a5789ec5e8fe1fae155bb98
Irena Omelana <124605706+IrenOm@users.noreply.github.com>
2023-03-29 20:30:05 +0300

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,106 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var age = 38
+var population = 8_000_000_000_000
+
+var str1 = """
+I like
+Swift
+language
+"""
+print(str1)
+
+let character = "Daphne"
+
+var playerName = "Irena"
+print(playerName)
+
+playerName = "Olga"
+print(playerName)
+
+playerName = "Agris"
+print(playerName)
+
+let managerName = "Michel Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "Be lucky"
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = " :) You win! "
+let quote = "Then he tappedca sign saying \"Believe\" and walked away"
+
+print(actor.count)
+
+let nameLength = dogBreed.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+let score = 10
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+counter %= 4
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
+
+let number1 = 0.1 + 0.2
+print(number)
+
+var name = "Ivan"
+var number2 = 4
+var floatingNumber = 4.5
+var doubleNumber = 5.4
+var someCharacter = "a"
+var trueOrFalse = true
+
+name
+number
+floatingNumber
+doubleNumber
+someCharacter
+trueOrFalse
+
+let goodDogs = true
+let gameOver = false
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAutenticated = false
+isAutenticated = !isAutenticated
+print(isAutenticated)
+isAutenticated = !isAutenticated
+print(isAutenticated)
+
+var dayOrNight = false
+print(dayOrNight)
+dayOrNight.toggle()
+print(dayOrNight)
+
+
